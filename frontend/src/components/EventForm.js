3ab3d0427e87b538a780dad8903d4a75bc702cd3
Christian Amauger <christian@amauger>
2023-06-19 18:42:59 -0400

    adding css and post for EventForm

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import './EventForm.css';
+import axios from 'axios';
 
 function EventForm() {
   const [name, setName] = useState('');
@@ -8,35 +10,55 @@ function EventForm() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [timezone, setTimezone] = useState('UTC');
+  const [message, setMessage] = useState(''); // New state variable for the success message
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // TODO: Send a POST request to the backend
+
+    // Send a POST request to the backend
+    axios.post('http://localhost:3000/events', {
+      name,
+      description,
+      startDate: startDate.toISOString(),
+      endDate: endDate.toISOString(),
+      timezone,
+    })
+    .then((response) => {
+      console.log(response);
+      setMessage('Event created successfully!');
+    })
+    .catch((error) => {
+      console.error(error);
+      /// Show an error message
+      setMessage('An error occurred while creating the event.');
+    });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
+    <form onSubmit={handleSubmit} className="form">
+      <label className="label">
         Name:
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="input" />
       </label>
-      <label>
+      <label className="label">
         Description:
-        <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
+        <textarea value={description} onChange={(e) => setDescription(e.target.value)} className="textarea" />
       </label>
-      <label>
+      <label className="label">
         Start Date:
-        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} className="datepicker" />
       </label>
-      <label>
+      <label className="label">
         End Date:
-        <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} />
+        <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} className="datepicker" />
       </label>
-      <label>
+      <label className="label">
         Timezone:
-        <input type="text" value={timezone} onChange={(e) => setTimezone(e.target.value)} />
+        <input type="text" value={timezone} onChange={(e) => setTimezone(e.target.value)} className="input" />
       </label>
-      <button type="submit">Create Event</button>
+      <button type="submit" className="button">Create Event</button>
+      {message && <p>{message}</p>} {/* Show the success or error message */}
+
     </form>
   );
 }
