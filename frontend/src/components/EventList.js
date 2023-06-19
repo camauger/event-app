c45b8e778598c4fa567e3a4162a1d34fe84d80b3
Christian Amauger <christian@amauger>
2023-06-19 18:22:10 -0400

    adding timezones

--- frontend/src/components/EventList.js
+++ frontend/src/components/EventList.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchEvents } from '../redux/eventsSlice';
+import { utcToZonedTime, format } from 'date-fns-tz';
 
 function EventList() {
   const dispatch = useDispatch();
@@ -16,7 +17,9 @@ function EventList() {
         <li key={event.id}>
           <h2>{event.name}</h2>
           <p>{event.description}</p>
-          <p>{event.startDate} to {event.endDate} ({event.timezone})</p>
+          <p>
+            {format(utcToZonedTime(event.startDate, event.timezone), 'Pp')} to {format(utcToZonedTime(event.endDate, event.timezone), 'Pp')} ({event.timezone})
+          </p>
         </li>
       ))}
     </ul>
